9f9edcd7ce2b28d7075c67bc9cf5448753901c83
Roberto Maldonado <robertomaldonado@users.noreply.github.com>
2024-09-19 19:01:45 -0500

    Time: 684 ms (48.14%), Space: 31.3 MB (32.58%) - LeetHub

---
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-arraywith_no_list.py
@@ -0,0 +1,26 @@
+"""
+Input: nums = [2,3,1,0]
+Output: 2
+Approach use sum array: 
+[2,  3,  1,  0]
+[2,  5,  6,  6]
+Iter 1. 
+[2 | 5,  6,  6]  .. 2 > 6 - 2 ? 0
+Iter 2.
+[2,  5 |  6,  6] .. 5 > 6 - 5 ? 1
+iter 3.
+[2,  5, 6 | 6] ... 6 >= 6-6?  1
+count 2
+"""
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        sums = [nums[0]]
+        for i in range(1, len(nums)):
+            sums.append(nums[i] + sums[-1])
+        ans = 0
+        for i in range(len(nums)-1):
+            left_sum, right_sum = sums[i] , sums[-1] - sums[i]
+            if left_sum >= right_sum:
+                ans += 1
+        return ans
+        
