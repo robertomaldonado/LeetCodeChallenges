1d84864c5e153592567ce872bfe140aab356c4b1
Roberto Maldonado <robertomaldonado@users.noreply.github.com>
2024-10-01 17:47:33 -0500

    Time: 33 ms (69.49%), Space: 16.4 MB (60.22%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(num_as_list)):
             if num_as_list[i] == "6":
                 num_as_list[i] = "9"
-                break
-        return int("".join(num_as_list))
+                return int("".join(num_as_list))
+        return num
 
         
