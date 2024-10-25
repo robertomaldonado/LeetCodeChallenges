6f4ac34c4c7be4a9e674149ae98dd763f8050dc1
Roberto Maldonado <robertomaldonado@users.noreply.github.com>
2024-10-25 15:16:06 -0500

    Time: 0 ms (100%), Space: 17.7 MB (90.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,30 +1,18 @@
-"""
-[1,2,3,4,5,6,7,8,9] nums
- ^       ^       ^
- 0       4       8
- l       m       r
-
-target = 7 
-if nums[4] > target, 9 > 7?:
-
-[1,2,3,4,5,6,7,8,9] nums
-         ^   ^   ^
-         4       8
-         l       r
-"""
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums)==1 and nums[0] == target:
-            return 0
         left = 0
-        right = len(nums)-1
-
-        while right > left:
-            mid = (right+left)//2
-            if nums[mid] == target:
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            num = nums[mid]
+            
+            if num == target:
                 return mid
-            elif nums[mid] < target: #move to the right
-                left = mid + 1
-            else: #move to the left
+            
+            if num > target:
                 right = mid - 1
+            else:
+                left = mid + 1
+        
         return -1
