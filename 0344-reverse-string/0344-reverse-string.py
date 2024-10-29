0bb16a558406b9f3a74405eb47d8a7be19b84e52
Roberto Maldonado <robertomaldonado@users.noreply.github.com>
2024-10-29 16:02:14 -0500

    Time: 2 ms (51.61%), Space: 22 MB (16.81%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,14 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        So we have: hello then we return olleh
-        define pointers at 0 and len of s
-        we swap, then advance. Consider that i,j cannot exceed each other
-        """
-        i, j = 0, len(s)-1
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i+=1
-            j-=1
+        right = 0
+        left = len(s)-1
+        while left > right: 
+            s[left], s[right] = s[right], s[left]
+            left -=1 
+            right +=1
         
